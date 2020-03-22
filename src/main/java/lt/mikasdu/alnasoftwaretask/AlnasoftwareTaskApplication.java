18478dae684251f30b1cef8497d9cad8124880d6
mikasdu <mdudzevicius@gmail.com>
2020-03-22 14:37:54 +0200

    init

---
+++ src/main/java/lt/mikasdu/alnasoftwaretask/AlnasoftwareTaskApplication.java
@@ -0,0 +1,13 @@
+package lt.mikasdu.alnasoftwaretask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlnasoftwareTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlnasoftwareTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lt/mikasdu/alnasoftwaretask/AlnasoftwareTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.mikasdu.alnasoftwaretask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlnasoftwareTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
